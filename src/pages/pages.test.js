d5c8bdb154967904f59610c5de3175dd9e8e9137
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-04-27 15:26:49 -0500

    All tests completed

--- src/pages/pages.test.js
+++ src/pages/pages.test.js
@@ -1,23 +1,23 @@
 import renderer from 'react-test-renderer';
 import Quotes from './quotes';
-import CalculatorP from './calculatorP';
 import Home from './home';
+import Calculator from './CalculatorP';
 import Navbar from './Navbar';
 
 describe('Pages renders correctly', () => {
-  it('renders correctly', () => {
+  it('Quotes page renders correctly', () => {
     const tree = renderer.create(<Quotes />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('renders correctly', () => {
-    const tree = renderer.create(<CalculatorP />).toJSON();
+  it('Calculator page renders correctly', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('renders correctly', () => {
+  it('Home page renders correctly', () => {
     const tree = renderer.create(<Home />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('renders correctly', () => {
+  it('Navbar renders correctly', () => {
     const tree = renderer.create(<Navbar />).toJSON();
     expect(tree).toMatchSnapshot();
   });
