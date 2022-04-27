b4919c2167155d24b71105c9b88f53f9e46d2a84
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-04-27 16:20:13 -0500

    Operate simulation added

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 import Calculator from './pages/CalculatorP';
+import operate from './logic/operate';
 
 describe('App interaction', () => {
   it('renders the home page', () => {
@@ -22,5 +23,23 @@ describe('App interaction', () => {
     expect(key1.value).toBe('1');
     fireEvent.click(key1);
     expect(display.innerHTML).toBe('1');
+  });
+
+  it('Simulate User interaction doing a sum of 8 + 9', () => {
+    render(<Calculator />);
+    const key8 = screen.getByText('8');
+    const key9 = screen.getByText('9');
+    const keyPlus = screen.getByText('+');
+    const keyEqual = screen.getByText('=');
+    const display = document.querySelector('.display');
+    expect(key8.value).toBe('8');
+    expect(key9.value).toBe('9');
+    expect(keyPlus.value).toBe('+');
+    expect(operate(key8.value, key9.value, keyPlus.value)).toBe('17');
+    fireEvent.click(key8);
+    fireEvent.click(keyPlus);
+    fireEvent.click(key9);
+    fireEvent.click(keyEqual);
+    expect(display.innerHTML).toBe('17');
   });
 });
